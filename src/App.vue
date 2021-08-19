fb1bb374a0dc2f87413457fb688b27b4ef9e65e7
royalknight56 <34654733+royalknight56@users.noreply.github.com>
2021-08-19 15:44:13 +0800

    feat: delete default apps

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <!--
  * @Author: zhangweiyuan-Royal
- * @LastEditTime: 2021-08-18 17:51:28
+ * @LastEditTime: 2021-08-19 15:43:04
  * @Description: 
  * @FilePath: /myindex/src/App.vue
 -->
@@ -14,21 +14,9 @@
 import { appList } from "./components/appconfig";
 import WinVue from "./components/win.vue";
 
-import computer from "./assets/computer.ico"
-import Test2Vue from "./apps/Test2.vue";
+// import computer from "./assets/computer.ico"
 
 
-
-setTimeout(()=>{
-  appList.push({
-        name: '我的电脑',
-        apptemp: 'Test1',
-        icon:computer,
-        width:400,
-        height:400,
-        tmp:Test2Vue
-    })
-},5000)
 </script>
 <style scoped>
 .outer{
--- src/components/appconfig.ts
+++ src/components/appconfig.ts
@@ -1,6 +1,6 @@
 /*
  * @Author: zhangweiyuan-Royal
- * @LastEditTime: 2021-08-18 19:27:09
+ * @LastEditTime: 2021-08-19 15:41:38
  * @Description: 
  * @FilePath: /myindex/src/components/appconfig.ts
  */
@@ -14,80 +14,82 @@ interface appInfo{
     tmp:DefineComponent<{}, {}, any>
 }
 
-import computer from "../assets/computer.ico"
-import beat from "../assets/beat.ico"
-import brow from "../assets/浏览器.png"
-import term from "../assets/term.ico"
-import vscode from "../assets/vscode.png"
-// import img1 from "../assets/term.ico"
-import GitHub from "../assets/GitHub.png"
 import { UnwrapNestedRefs } from "@vue/reactivity";
-import { reactive } from "vue"
 
-import Test1 from "../apps/Test1.vue"
-import Test2 from "../apps/Test2.vue"
-import Test3 from "../apps/Test3.vue"
+// import computer from "../assets/computer.ico"
+// import beat from "../assets/beat.ico"
+// import brow from "../assets/浏览器.png"
+// import term from "../assets/term.ico"
+// import vscode from "../assets/vscode.png"
+// // import img1 from "../assets/term.ico"
+// import GitHub from "../assets/GitHub.png"
 
-import app_console from "../apps/app_console.vue"
-import app_vscode from "../apps/app_vscode.vue"
-import Adm_loading from "../apps/Adm_loading.vue"
-import GitStars from "../apps/GitStars.vue"
+// import { reactive } from "vue"
+
+// import Test1 from "../apps/Test1.vue"
+// import Test2 from "../apps/Test2.vue"
+// import Test3 from "../apps/Test3.vue"
+
+// import app_console from "../apps/app_console.vue"
+// import app_vscode from "../apps/app_vscode.vue"
+// import Adm_loading from "../apps/Adm_loading.vue"
+// import GitStars from "../apps/GitStars.vue"
 
 
 let appList:UnwrapNestedRefs<Array<appInfo>> = shallowReactive([
-    {
-        name: '我的电脑',
-        apptemp: 'Test1',
-        icon:computer,
-        width:400,
-        height:400,
-        tmp:Test1
-    }, {
-        name: '版本信息',
-        apptemp: 'Test2',
-        icon:beat,
-        width:100,
-        height:100,
-        tmp:Test2
-    }, {
-        name: '浏览器',
-        apptemp: 'Test3',
-        icon:brow,
-        width:600,
-        height:500,
-        tmp:Test3
-    },{
-        name: '终端',
-        apptemp: 'app_console',
-        icon:term,
-        width:400,
-        height:400,
-        tmp:app_console
+    // {
+    //     name: '我的电脑',
+    //     apptemp: 'Test1',
+    //     icon:computer,
+    //     width:400,
+    //     height:400,
+    //     tmp:Test1
+    // }, {
+    //     name: '版本信息',
+    //     apptemp: 'Test2',
+    //     icon:beat,
+    //     width:100,
+    //     height:100,
+    //     tmp:Test2
+    // }, {
+    //     name: '浏览器',
+    //     apptemp: 'Test3',
+    //     icon:brow,
+    //     width:600,
+    //     height:500,
+    //     tmp:Test3
+    // },{
+    //     name: '终端',
+    //     apptemp: 'app_console',
+    //     icon:term,
+    //     width:400,
+    //     height:400,
+    //     tmp:app_console
 
-    },{
-        name: 'vscode',
-        apptemp: "app_vscode",
-        icon:vscode,
-        width:600,
-        height:500,
-        tmp:app_vscode
-    },
-    {
-        name: '后台管理',
-        apptemp: "Adm_loading",
-        icon:term,
-        width:300,
-        height:400,
-        tmp:Adm_loading
-    },
-    {
-        name: '点个star',
-        apptemp: "GitStars",
-        icon:GitHub,
-        width:300,
-        height:400,
-        tmp:GitStars
-    },
+    // },{
+    //     name: 'vscode',
+    //     apptemp: "app_vscode",
+    //     icon:vscode,
+    //     width:600,
+    //     height:500,
+    //     tmp:app_vscode
+    // },
+    // {
+    //     name: '后台管理',
+    //     apptemp: "Adm_loading",
+    //     icon:term,
+    //     width:300,
+    //     height:400,
+    //     tmp:Adm_loading
+    // },
+    // {
+    //     name: '点个star',
+    //     apptemp: "GitStars",
+    //     icon:GitHub,
+    //     width:300,
+    //     height:400,
+    //     tmp:GitStars
+    // },
 ])
 
 export {appList,appInfo}

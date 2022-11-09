b172e9b612982da5a16385b410eb2b31790af845
royalknight56 <2930470134@qq.com>
2022-11-09 22:30:47 +0800

    feat: 改进

--- src/packages/window/structure/ContextMenu.vue
+++ src/packages/window/structure/ContextMenu.vue
@@ -6,34 +6,28 @@
   Need CodeReview 
 -->
 <template>
-    <div class="menu_outer" :style="cusStyle" :class="{ defOver: IPC.ifTopDown }">
-        <div class="menulist" v-for="item in IPC.menuList" @click="callFunc(item)">{{ item.name }}</div>
+    <div class="menu_outer" :style="menuPos" :class="{ closeBorder: ContextMenu.ifTopDown }">
+        <div class="menulist" v-for="item in ContextMenu.menuList" @click="callFunc(item)">{{ item.name }}</div>
     </div>
 </template>
 <script lang="ts" setup>
 import { computed, inject, reactive, ref } from "vue";
-import type {menuItem}from "@libs/ContextMenu";
-
-import {System} from '@libs/System'
-
+import type { menuItem } from "@libs/ContextMenu";
+import { System } from '@libs/System'
 let system = <System>inject('system')
-
-let IPC = system.ContextMenu
-
-
-let cusStyle = ref<any>({
+let ContextMenu = system.ContextMenu
+let menuPos = ref<any>({
 })
-cusStyle.value.left = computed(() => system.ContextMenu.x.value + 'px')
-cusStyle.value.top = computed(() => system.ContextMenu.y.value + 'px')
-
-cusStyle.value.visibility = computed(() => system.ContextMenu.ifShow.value?"visible":"hidden" )
-
-function callFunc(item:menuItem) {
+menuPos.value.left = computed(() => system.ContextMenu.x.value + 'px')
+menuPos.value.top = computed(() => system.ContextMenu.y.value + 'px')
+menuPos.value.visibility = computed(() => system.ContextMenu.ifShow.value ? "visible" : "hidden")
+function callFunc(item: menuItem) {
     item.click()
 }
 </script>
 <style scoped>
 @import '../../main.css';
+
 .menu_outer {
     position: absolute;
     background-color: rgb(230, 230, 230);
@@ -42,18 +36,19 @@ function callFunc(item:menuItem) {
     box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
     z-index: 102;
 }
-.defOver {
+
+.closeBorder {
     transform: translateY(-100%);
 }
-.menulist{
+
+.menulist {
     cursor: pointer;
     width: 80px;
-    
-    /* padding: 4px; */
     padding: 4px 30px;
     font-size: 12px;
 }
-.menulist:hover{
+
+.menulist:hover {
     background-color: white;
     color: black;
 }

e6181fbc74887f3d86c7e7ca0c394c414f531d46
royalknight56 <2930470134@qq.com>
2023-11-04 11:33:18 +0800

    feat: loading button

--- src/packages/components/WinButton.vue
+++ src/packages/components/WinButton.vue
@@ -1,8 +1,22 @@
 <template>
-  <button class="win-button">
+  <button
+    class="win-button"
+    :class="{
+      'win-button--loading': loading,
+    }"
+    :disabled="loading"
+  >
     <slot></slot>
   </button>
 </template>
+<script lang="ts" setup>
+defineProps({
+  loading: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
 <style scoped lang="scss">
 .win-button {
   background-color: var(--color-ui-gray);
@@ -18,6 +32,25 @@
     border-width: 2px;
     border-color: var(--color-blue);
     background-color: var(--color-light-blue);
+  }
+}
+.win-button--loading {
+  animation: loading 1s infinite;
+}
+.win-button--loading:hover {
+  border-width: 1px;
+  border-color: rgba(118, 118, 171, 0.355);
+  background-color: var(--color-light-blue);
+}
+@keyframes loading {
+  0% {
+    background-color: var(--color-ui-gray);
+  }
+  50% {
+    background-color: var(--color-light-blue);
+  }
+  100% {
+    background-color: var(--color-ui-gray);
   }
 }
 </style>

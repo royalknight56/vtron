80c76130ac91ed102655cc783ed07a22673a8dee
royalknight56 <2930470134@qq.com>
2024-03-15 10:28:24 +0800

    feat: 优化

--- packages/vtron/src/packages/application/OpenWiteDialog.vue
+++ packages/vtron/src/packages/application/OpenWiteDialog.vue
@@ -22,14 +22,13 @@
 <script lang="ts" setup>
 import { inject, ref, onMounted } from 'vue';
 import { i18n, BrowserWindow } from '@packages/ui';
-import { VtronFileWithoutContent, useSystem } from '@packages/kernel';
+import { useSystem } from '@packages/kernel';
 
 // import { VtronFileWithoutContent, i18n } from '@packages/plug';
 
 const window: BrowserWindow | undefined = inject('browserWindow');
 const sys = useSystem();
-const file = ref<VtronFileWithoutContent | null>();
-file.value = await sys?.fs.stat(window?.config.content);
+
 type TempOpener = {
   name: string;
   icon: string;

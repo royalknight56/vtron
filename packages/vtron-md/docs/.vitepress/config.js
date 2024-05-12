7dbdfd383bc60188c52fc3cd1326a361ddd38c70
royalknight56 <2930470134@qq.com>
2024-05-12 18:11:11 +0800

    feat: base

--- packages/vtron-md/docs/.vitepress/config.js
+++ packages/vtron-md/docs/.vitepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: 'Vtron',
-  base: './',
+  base: '/doc/',
   description: 'Just playing around',
   head: [
     [
@@ -28,7 +28,7 @@ module.exports = {
     en: {
       label: 'English',
       lang: 'en-US',
-      link: './en/', // default /en/ -- shows on navbar translations menu, can be external
+      link: '/en/', // default /en/ -- shows on navbar translations menu, can be external
     },
   },
   // 配置默认主题
@@ -37,69 +37,69 @@ module.exports = {
     lastUpdated: 'Last Updated', // string | boolean
     aside: true,
     nav: [
-      { text: '主页', link: './' },
-      { text: '教程', link: './guide/how-to-use-vtron.md' },
-      { text: 'API', link: './api/BrowserWindow.md' },
-      { text: 'VtronPlus', link: './plus/VtronPlus.md' },
-      { text: '更新', link: './update/README.md' },
+      { text: '主页', link: '/' },
+      { text: '教程', link: '/guide/how-to-use-vtron.md' },
+      { text: 'API', link: '/api/BrowserWindow.md' },
+      { text: 'VtronPlus', link: '/plus/VtronPlus.md' },
+      { text: '更新', link: '/update/README.md' },
       { text: 'GitHub', link: 'https://github.com/royalknight56/vtron' },
       { text: 'Demo', link: 'http://vtron.site' },
     ],
     sidebar: {
-      './api/': [
+      '/api/': [
         {
           text: 'System',
-          link: './api/System.md',
+          link: '/api/System.md',
         },
         {
           text: '系统的配置项',
-          link: './api/System-option.md',
+          link: '/api/System-option.md',
         },
         {
           text: 'BrowserWindow',
-          link: './api/BrowserWindow.md',
+          link: '/api/BrowserWindow.md',
         },
         {
           text: 'fs',
-          link: './api/fs.md',
+          link: '/api/fs.md',
         },
         {
           text: 'path',
-          link: './api/path.md',
+          link: '/api/path.md',
         },
         {
           text: 'event',
-          link: './api/event.md',
+          link: '/api/event.md',
         },
         {
           text: 'Dialog',
-          link: './api/Dialog.md',
+          link: '/api/Dialog.md',
         },
         {
           text: 'Tary',
-          link: './api/Tary.md',
+          link: '/api/Tary.md',
         },
         {
           text: 'Shell',
-          link: './api/Shell.md',
+          link: '/api/Shell.md',
         },
         {
           text: 'Notify',
-          link: './api/Notify.md',
+          link: '/api/Notify.md',
         },
         {
           text: '其他',
-          link: './api/Other.md',
+          link: '/api/Other.md',
         },
         {
           text: '插件开发',
-          link: './api/Plugin.md',
+          link: '/api/Plugin.md',
         },
       ],
-      './guide/': [
-        { text: '如何使用', link: './guide/how-to-use-vtron.md' },
-        { text: '功能导航', link: './guide/roadmap.md' },
-        { text: '常见问题', link: './guide/qa.md' },
+      '/guide/': [
+        { text: '如何使用', link: '/guide/how-to-use-vtron.md' },
+        { text: '功能导航', link: '/guide/roadmap.md' },
+        { text: '常见问题', link: '/guide/qa.md' },
         // { text: 'What is vtron?', link: '/guide/what-is-vtron.md' },
       ],
     },

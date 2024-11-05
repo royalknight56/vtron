793fa1df4a3edfe75608d80e06abe519b0c03055
jahv <me@cjahv.com>
2024-11-05 16:16:33 +0000

    feat(vtron): 添加测试 UI 按钮应用
    - 在 App.vue 中添加了新的测试 UI 按钮应用
    - 新增 TestUiButton.vue 组件，用于展示不同长度的按钮
    - 通过 WinButtonVue 组件实现按钮的点击功能
    - 应用支持拖拽功能

---
+++ packages/vtron/src/apps/TestUiButton.vue
@@ -0,0 +1,19 @@
+<!--
+ * @Author: Jahv
+ * @LastEditTime: 2024-11-06 00:06:33
+ * @Description: 测试按钮
+-->
+<template>
+  <div class="outer" v-dragable>
+    <p style="padding: 10px">
+      <WinButtonVue @click="">Test</WinButtonVue>
+      <br />
+      <WinButtonVue @click="">Test111111111111</WinButtonVue>
+      <br />
+      <WinButtonVue @click="">长长长长长长长长长长长长长长长长长长</WinButtonVue>
+    </p>
+  </div>
+</template>
+<script lang="ts" setup>
+import { vDragable, WinButtonVue } from '@packages/plug';
+</script>
